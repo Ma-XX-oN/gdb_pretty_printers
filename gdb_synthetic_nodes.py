3ed0d336f20795885bb85b51284d35005cd64efa
Adrian Hawryluk <adrianh.bsc@gmail.com>
2025-10-17 12:53:47 -0400

    - Added more detailed comments - Fixed issue finding the enums for the type when looking at more complex types.

--- gdb_synthetic_nodes.py
+++ gdb_synthetic_nodes.py
@@ -1,7 +1,14 @@
-# Helpers for creating/manipulating synthetic tags using enum values
+"""
+Helpers for creating/manipulating synthetic tag types for gdb pretty-printing.
+
+The gdb is great, but to make pretty printers for types lacks the ability to
+generate synthetic nodes to organise the data.  This module addresses that by
+making values of types that don't make sense in the wild so are safe to use and
+are capable of being reverted back to their original type quickly and easily.
+"""
+
 import gdb
 import re
-from gdb_logger import log
 
 def make_enums_tag(val, enum_tuple):
   """Create a synthetic tag value for gdb.
@@ -15,18 +22,18 @@ def make_enums_tag(val, enum_tuple):
   val : gdb.Value
     The original value (usually the object instance).
   enum_tuple : tuple[int, ...]
-    Tuple of small integers to encode in the synthetic tag.
+    Tuple of integers to encode in the synthetic tag.
 
   Returns
   -------
   gdb.Value
-    The synthetic tag value (address casted to a quad-pointer type).
+    The synthetic tag value (address casted to a pointer of a N dimensional
+    array of quad-pointer type).
   """
   base_type = val.type.pointer()
   array_type = base_type
   for enum in reversed(enum_tuple):
     array_type = array_type.array(enum - 1)
-  log(f"array_type: {array_type}\n")
   ptr_type = array_type.pointer().pointer().pointer().pointer()
   return val.address.cast(ptr_type)
 
@@ -52,9 +59,8 @@ def extract_enums_tag(val):
 def recover_value(val):
   """Recover the original value from a synthetic tag.
 
-  This attempts to retrieve the base type from the quad-pointer type
-  representation used by make_enums_tag and returns the dereferenced
-  original object value.
+  This retrieve the base type from the synthetic type representation used by
+  make_enums_tag and returns the dereferenced original object value.
 
   Parameters
   ----------
@@ -66,6 +72,6 @@ def recover_value(val):
   gdb.Value
     The original value with its proper base type.
   """
-  i = str(val.type).index(' ')
+  i = str(val.type).index(' *(****)[')
   base_type = str(val.type)[0:i]
   return val.cast(gdb.lookup_type(base_type).pointer()).dereference()
