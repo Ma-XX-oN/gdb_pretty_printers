190a471498bfda1e2dcad1cac51f309c47ba0627
Adrian Hawryluk <adrianh.bsc@gmail.com>
2025-10-17 16:58:52 -0400

    - Added upgrades to the logger.

--- gdb_logger.py
+++ gdb_logger.py
@@ -1,7 +1,16 @@
 import gdb
 
-logger = open("gdb_log.txt", "w")
+logger = None
+def logging_on(log_filename):
+  global logger
+  logger = open(log_filename, "w")
 
+def _gdb_logging_enabled():
+  try:
+    return gdb.parameter("logging enabled")
+  except gdb.error:
+    return False
+  
 def log(msg):
   """Log a message to gdb console.
 
@@ -10,11 +19,14 @@ def log(msg):
   msg : str
       The message to log.
   """
-  log_msg = f"[LOGGER]{"" if (msg[0] == "[") else " "}{msg}\n"
-  gdb.write(log_msg, gdb.STDOUT)
-  gdb.flush()
+  gdb_logger = _gdb_logging_enabled()
+  if gdb_logger or logger:
+    log_msg = f"[LOGGER]{"" if (msg[0] == "[") else " "}{msg}\n"
+
+  if gdb_logger:  
+    gdb.write(log_msg, gdb.STDOUT)
+    gdb.flush()
 
   if logger:  
     logger.write(log_msg)
     logger.flush()
-
