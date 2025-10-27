5f3320ac16688207eba6b36d362ccfed54fd9c77
Adrian Hawryluk <adrianh.bsc@gmail.com>
2025-10-27 14:21:06 -0400

    fix(maintenance):  General maintenance and fixed/added some functionality.
    Maintenance:
    - Prefixing _ to private names.
    - Adding types to sigs.
    - Added gdb.ValuePrinter as base class to all printers.
    Fixes:
    - Arrays can now be tagged.
    - Summary logic was wrong.
    - Using RawPrinter more consistently so that it has a summary when not top-level.
    Functionality:
    - Added MessagePrinter which might be useful.
    - Added ArrayPrinter.
    - Adjusted logic in _lookup_type to allow working with arrays.

--- gdb_printers.py
+++ gdb_printers.py
@@ -7,6 +7,7 @@ import gdb
 import re
 import traceback
 
+from typing import Tuple
 from gdb_logger import log
 from gdb_synthetic_nodes import make_enums_tag, extract_enums_tag, recover_value
 
@@ -20,7 +21,7 @@ def add_printer(type_name, printer):
   ----------
   type_name : string
     The type name to match against to pretty print
-      
+
   printer : dict
       "summary" - lambda(v) : string (optional)
         - Raw summary.
@@ -38,7 +39,7 @@ def add_printer(type_name, printer):
             - If not specified, will show nothing for the summary.
           "nodes" - list (optional)
             - An even number of elements, where:
-              - 1st element is the name of the element, and 
+              - 1st element is the name of the element, and
               - 2nd element is a lambda(v) : {gdb.Value | any}
           "node" - class object (optional)
             - For complex views, it may be necessary to write a full blown class
@@ -58,7 +59,7 @@ def add_re_printer(type_re, printer):
   ----------
   type_name : string
     The regex to match against the type to pretty print
-      
+
   printer : dict
       "summary" - lambda(v) : string (optional)
         - Raw summary.
@@ -76,7 +77,7 @@ def add_re_printer(type_re, printer):
             - If not specified, will show nothing for the summary.
           "nodes" - list (optional)
             - An even number of elements, where:
-              - 1st element is the name of the element, and 
+              - 1st element is the name of the element, and
               - 2nd element is a lambda(v) : {gdb.Value | any}
           "node" - class object (optional)
             - For complex views, it may be necessary to write a full blown class
@@ -101,14 +102,19 @@ def _match_printer(type_str):
 
 # Synthetic node tags
 # Each tag is a tuple of integers; the first integer indicates the kind of node.
-STATIC_ENUM = (0,)
-RAW_ENUM = (1,)
-CHUNK_ENUM_ = 2
-def CHUNK_ENUM(offset, chunk_size):
-  return (2, offset, chunk_size)
-VIEW_ENUM_ = 3
-def VIEW_ENUM(view_index):
-  return (VIEW_ENUM_ + view_index,)
+_STATIC_ENUM_I = 0
+_STATIC_ENUM = (_STATIC_ENUM_I,)
+_RAW_ENUM_I = 1
+_RAW_ENUM = (_RAW_ENUM_I,)
+_CHUNK_ENUM_I = 2
+def _CHUNK_ENUM(offset, chunk_size) -> Tuple[int, ...]:
+  return (_CHUNK_ENUM_I, offset, chunk_size)
+_MSG_ENUM_I = 3
+def _MSG_ENUM(string) -> Tuple[int, ...]:
+  return (_MSG_ENUM_I, *list(string.encode("utf-8")))
+_VIEW_ENUM_I = 4
+def _VIEW_ENUM(view_index) -> Tuple[int, ...]:
+  return (_VIEW_ENUM_I + view_index,)
 
 def INCOMPLETE():
   raise NotImplementedError("INCOMPLETE: function not implemented yet")
@@ -169,7 +175,7 @@ def _is_forward(it):
       return True
   except gdb.error:
     return False
-  
+
 def _is_bidirectional(it):
   try:
     with GdbConvenienceVars('pp_it', it):
@@ -230,7 +236,7 @@ def get_c_range_and_size(val, begin_member_name, end_member_name, size_member_na
 
   if size is not None:
     return (begin, end, size)
-  
+
   return (begin, end)
 
 class GdbConvenienceVars:
@@ -246,7 +252,7 @@ class GdbConvenienceVars:
     ----------
     name_value_pairs : List[ Tuple[string, gdb.Value], ... ]
         A list of name value tuples to use as convenience variables.
-    
+
     Raises
     ------
     ValueError
@@ -298,7 +304,7 @@ def emit_chunked_elements(c_range_and_size, chunk_size=16):
       length = int(size) if size is not None else _to_int(e_ptr - b_ptr)
       for i in range(0, length, chunk_size):
         n = chunk_size if i + chunk_size <= length else (length - i)
-        yield f'[{i}..{i+n-1}]', make_enums_tag(b_ptr + i, CHUNK_ENUM(i, n))
+        yield f'[{i}..{i+n-1}]', make_enums_tag(b_ptr + i, _CHUNK_ENUM(i, n))
       return
 
     # 2) Random-access iterators via C++ evaluator
@@ -308,7 +314,7 @@ def emit_chunked_elements(c_range_and_size, chunk_size=16):
         for i in range(0, length, chunk_size):
           n = chunk_size if i + chunk_size <= length else (length - i)
           it_i = gdb.parse_and_eval(f'$_b + {i}')
-          yield f'[{i}..{i+n-1}]', make_enums_tag(it_i, CHUNK_ENUM(i, n))
+          yield f'[{i}..{i+n-1}]', make_enums_tag(it_i, _CHUNK_ENUM(i, n))
       return
 
     # 3) Forward / bidirectional: scan from begin to end (or until `size`)
@@ -340,7 +346,7 @@ def emit_chunked_elements(c_range_and_size, chunk_size=16):
         if steps == 0:  # nothing advanced (size too big or already at end)
           break
 
-        yield f'[{i}..{i+steps-1}]', make_enums_tag(start_it, CHUNK_ENUM(i, steps))
+        yield f'[{i}..{i+steps-1}]', make_enums_tag(start_it, _CHUNK_ENUM(i, steps))
         i += steps
 
   except gdb.error as e:
@@ -396,31 +402,29 @@ def summary(named=False, show_type=True, show_char_as_int=True):
   else:
     field_entry = lambda v, f: val_to_str(v[f.name])
 
-  def summary(val):
+  def summary_fn(val : gdb.Value):
     try:
       fields = val.type.fields()
-      if len(fields) > 0:
-        summary = "{ "
-        field_count = 0
-        for i in range(len(fields)):
-          field = fields[i]
-          if (getattr(field, "is_base_class", False) is False and 0 != getattr(field, "bitpos", 0)):
-            summary += field_entry(val, field)
-            field_count += 1
-            break
+      summary = "{ "
+      field_count = 0
+      # get first non base class/static member
+      i = 0
+      for i in range(len(fields)):
+        field = fields[i]
+        if (getattr(field, "is_base_class", False) is False and getattr(field, "bitpos", False)) is not False:
+          summary += field_entry(val, field)
+          field_count += 1
+          break
 
-        for field_i in range(i+1, len(fields)):
-          field = fields[field_i]
-          if getattr(field, "is_base_class", False):
-            continue
-          if getattr(field, "bitpos", None) is not None:
-            summary += ", " + field_entry(val, field)
-            field_count += 1
-        
-        if (field_count):
-          summary += " }"
-        else:
-          summary = "{}"
+      # get rest non base class/static members
+      for field_i in range(i+1, len(fields)):
+        field = fields[field_i]
+        if (getattr(field, "is_base_class", False) is False and getattr(field, "bitpos", False)) is not False:
+          summary += ", " + field_entry(val, field)
+          field_count += 1
+
+      if (field_count):
+        summary += " }"
       else:
         summary = "{}"
     except Exception as e:
@@ -430,17 +434,18 @@ def summary(named=False, show_type=True, show_char_as_int=True):
     if show_type:
       summary = f"{val.type} {summary}"
     return summary
-  
-  return summary
+
+  return summary_fn
 
 def emit_raw_children(val):
   """ Emit raw children of a gdb.Value if possible """
   try:
-    for field in val.type.fields():
-      if getattr(field, "is_base_class", False):
-        yield f"{field.name} (base)", val.cast(gdb.lookup_type(field.name).reference())
-      elif getattr(field, "bitpos", None) is not None:
-        yield field.name, val[field.name] # non-static field
+    if val.type.code != gdb.TYPE_CODE_ARRAY:
+      for field in val.type.fields():
+        if getattr(field, "is_base_class", False):
+          yield f"{field.name} (base)", val.cast(gdb.lookup_type(field.name).reference())
+        elif getattr(field, "bitpos", None) is not None:
+          yield field.name, val[field.name] # non-static field
   except Exception as e:
     log(f"emit_raw_children exception: {e}\n  {traceback.format_exc()}")
     return
@@ -448,15 +453,16 @@ def emit_raw_children(val):
 def emit_static_children(val):
   """ Emit static children of a gdb.Value if any """
   try:
-    for field in val.type.fields():
-      if getattr(field, "bitpos", None) is not None:
-        continue
-      yield field.name, val[field.name]
+    if val.type.code != gdb.TYPE_CODE_ARRAY:
+      for field in val.type.fields():
+        if getattr(field, "bitpos", None) is not None:
+          continue
+        yield field.name, val[field.name]
     return
   except Exception as e:
     log(f"emit_raw_children exception: {e}\n  {traceback.format_exc()}")
     return
-  
+
   t = val.type.strip_typedefs()
   qual = t.tag or t.name
   if not qual:
@@ -465,7 +471,7 @@ def emit_static_children(val):
   for f in t.fields():
     if getattr(f, "is_base_class", False):
       continue
-    
+
     # Prefer is_static when available; otherwise fall back to "no bitpos ⇒ static"
     is_static = getattr(f, "is_static", None)
     if is_static is None:
@@ -506,7 +512,7 @@ def has_static(val):
   for f in t.fields():
     if getattr(f, "is_base_class", False):
       continue
-    
+
     # Prefer is_static when available; otherwise fall back to "no bitpos ⇒ static"
     is_static = getattr(f, "is_static", None)
     if is_static is None:
@@ -517,9 +523,28 @@ def has_static(val):
     return True
   return False
 
-class DefaultPrinter:
+class MessagePrinter(gdb.ValuePrinter):
+  """Allows a message to be on the rhs of =.  Not very useful I think as
+     ArrayPrinter will do the same with a python string.
+  """
+  def __init__(self, val : gdb.Value, string) -> None:
+    self.val = val
+    self.string = string
+
+  def num_children(self):
+    # Hmmmm. Never called.
+    log("Getting num_children")
+    return 0
+
+  def children(self):
+    yield "*This is only a message*", None
+
+  def to_string(self):
+    return self.string
+
+class DefaultPrinter(gdb.ValuePrinter):
   """Handler for default pretty-printing of structs/unions/classes"""
-  def __init__(self, val, printer = None):
+  def __init__(self, val : gdb.Value, printer = None):
     self.val = val
     self.printer = printer
 
@@ -529,7 +554,7 @@ class DefaultPrinter:
         if view['name'] == name:
           return view
     return None
-  
+
   def to_string(self):
     view_count = self.count_views()
     if self.printer is not None and 'default_view' in self.printer:
@@ -550,7 +575,7 @@ class DefaultPrinter:
         return default_view['summary'](self.val)
     # view has no summary, or no views/printer: show nothing
     return ""
-  
+
   def count_views(self):
     if self.printer is not None and 'views' in self.printer:
       log(f"count_views for {self.val.type} = {len(self.printer['views'])}")
@@ -561,7 +586,7 @@ class DefaultPrinter:
     if self.printer is not None and 'views' in self.printer and index < len(self.printer['views']):
       return self.printer['views'][index]['name']
     return f"View {index}"
-  
+
   def children(self):
     log(f"DefaultPrinter children for {self.val.type}\n{self.printer}")
 
@@ -581,8 +606,34 @@ class DefaultPrinter:
     else:
       # show default view at top-level
       if default_view is None:
-        yield f'<default_view "{default_view_name}" not defined>', \
-          self.val.cast(self.val.type.array(-1)) # prevents showing assignment or expanding node
+        # default view is misnamed
+        # IT'S PRETTY DAMN ANNOYING THAT I CAN'T GET A NODE WITH NO DROPDOWN OR
+        # AN `= ...` AFTER IT.
+
+        # No `= ...`, but still dropdown
+        # yield f"<default_view '{default_view_name}' not defined>", ''
+
+        # No `= ...`, but still dropdown and shows base class items without
+        # values?  Weird.  I guess UB.
+        # yield f"<default_view '{default_view_name}' not defined>", None
+        # yield f"<default_view '{default_view_name}' not defined>", (0,)
+
+        # Still dropdown
+        # yield '>ERROR<', \
+        #   make_enums_tag(self.val, _MSG_ENUM(f'default_view "{default_view_name}" not defined'))
+
+        # Still dropdown.  But since implemented with python string,
+        # ArrayPrinter at least limits the depth to be only one more level, but
+        # still reports:
+        #   Python Exception <class 'TypeError'>: Could not convert Python object: None.
+        # yield '>ERROR<', \
+        #   f'default_view "{default_view_name}" not defined'
+
+        # NOTE: Might be able to create my own gdb.Type which might have no
+        #       dropdown.  Might be able to use those types instead of these
+        #       numeric enums typed as a way to make views.
+        #       Need to mull that over for a bit.
+        pass
       else:
         log(f"top-level view for {self.val.type} = {default_view_name}")
         yield from ViewPrinter(self.val, default_view).children()
@@ -590,22 +641,53 @@ class DefaultPrinter:
     # Show static/raw/views views
     if has_static(self.val):
       log(f"has_static for {self.val.type}")
-      yield '<Static>', make_enums_tag(self.val, STATIC_ENUM)
+      yield '<Static>', make_enums_tag(self.val, _STATIC_ENUM)
 
     if view_count > 0:
       if default_view_name is not None:
         # show <Raw> tag if there are views and a default_view
-        yield '<Raw>', make_enums_tag(self.val, RAW_ENUM)
+        yield '<Raw>', make_enums_tag(self.val, _RAW_ENUM)
 
       # show views other than default_view
       for i in range(view_count):
         view_name = self.view_name(i)
         if view_name != default_view_name:
-          yield f"<{view_name}>", make_enums_tag(self.val, VIEW_ENUM(i))
+          yield f"<{view_name}>", make_enums_tag(self.val, _VIEW_ENUM(i))
 
-class StaticPrinter:
-  """Handler for static members only"""
-  def __init__(self, val):
+CHAR_TYPES = (gdb.lookup_type('char'), gdb.lookup_type('unsigned char'))
+
+class ArrayPrinter(gdb.ValuePrinter):
+  def __init__(self, val : gdb.Value) -> None:
+    self.val = val
+    self.python_string = False
+
+    t = self.val.type.strip_typedefs().unqualified()
+    self.low, self.high = self.val.type.range()
+    n = self.high - self.low + 1
+    if t.target().unqualified() in CHAR_TYPES:
+      self.summary = self.val.lazy_string(length=n)  # GDB handles quotes/escaping
+      if str(self.summary) == '<error: Cannot access memory at address 0x0>':
+        # This is a python string so don't quote it
+        self.summary = self.val.string(length=n)
+        self.python_string = True
+    elif n == 0:
+      self.summary = ""
+    else:
+      self.summary = f"[...] length = {n}"
+
+  def to_string(self):
+    return self.summary
+
+  def children(self):
+    if not self.python_string:
+      for i in range(self.low, self.high + 1):
+        yield f"[{i}]", self.val[i]
+    else:
+      yield f"Parent node is a temporary python string holder", None
+
+class StaticPrinter(gdb.ValuePrinter):
+  """Handler for static members"""
+  def __init__(self, val : gdb.Value):
     self.val = val
 
   def children(self):
@@ -614,20 +696,25 @@ class StaticPrinter:
   def to_string(self):
     return ""
 
-class RawPrinter:
-  """Handler for raw members only"""
-  def __init__(self, val):
+class RawPrinter(gdb.ValuePrinter):
+  """Handler for raw view of members"""
+  def __init__(self, val : gdb.Value, printer = None):
     self.val = val
+    self.printer = printer
 
   def children(self):
     yield from emit_raw_children(self.val)
 
   def to_string(self):
-    return ""
-  
-class ViewPrinter:
+    if self.printer is not None and 'summary' in self.printer:
+      summary = self.printer['summary'](self.val)
+    else:
+      summary = ""
+    return summary
+
+class ViewPrinter(gdb.ValuePrinter):
   """Handler for a specific view of members"""
-  def __init__(self, val, view):
+  def __init__(self, val : gdb.Value, view):
     self.val = val
     self.view = view
 
@@ -652,15 +739,15 @@ class ViewPrinter:
         log(f"ViewPrinter elements for {self.val.type}")
         yield from self.view['elements'](self.val)
     return
-  
+
   def to_string(self):
     if 'summary' in self.view and self.view['summary']:
       return self.view['summary'](self.val)
     return getattr(self.view, 'summary', '')
 
-class ChunkPrinter:
+class ChunkPrinter(gdb.ValuePrinter):
   """Handler for chunked elements"""
-  def __init__(self, val, offset, chunk_size):
+  def __init__(self, val : gdb.Value, offset, chunk_size):
     self.val = val
     self.offset = offset
     self.chunk_size = chunk_size
@@ -674,38 +761,46 @@ class ChunkPrinter:
 def _lookup_type(val):
   type_str = str(val.type)
   log(f"type: {type_str}")
-  
-  printer = _match_printer(type_str)
-  if printer is not None:
-    log(f"Matched printer for type: {type_str}")
-    return DefaultPrinter(val, printer)
-  
-  if val.type.code in (gdb.TYPE_CODE_STRUCT, gdb.TYPE_CODE_UNION):
-    log(f"DefaultPrinter for struct/union type: {type_str}")
-    return DefaultPrinter(val)
-  
+
   # Match synthetic node tags by looking for pointer pattern
   if '(****)' in type_str:
     enums = extract_enums_tag(val)
     actual_val = recover_value(val)
-    base_key = str(actual_val.type)
+    actual_val_type_str = str(actual_val.type)
+    printer = _match_printer(actual_val_type_str)
 
     log(f"enums: {enums}")
-    if enums == STATIC_ENUM:
+    if enums == _STATIC_ENUM:
       log(f"StaticPrinter for {actual_val.type}")
       return StaticPrinter(actual_val)
-    elif enums == RAW_ENUM:
+    elif enums == _RAW_ENUM:
       log(f"RawPrinter for {actual_val.type}")
-      return RawPrinter(actual_val)
-    elif enums[0] == CHUNK_ENUM_:
+      return RawPrinter(actual_val, printer)
+    elif enums[0] == _CHUNK_ENUM_I:
       log(f"ChunkPrinter for {actual_val.type} chunk size {enums[1]}")
       return ChunkPrinter(actual_val, offset=enums[1], chunk_size=enums[2])
+    elif enums[0] == _MSG_ENUM_I:
+      return MessagePrinter(actual_val, "".join(map(chr, enums[1:])))
     else:
       # everything else is a view
-      view_index = enums[0] - VIEW_ENUM_
+      view_index = enums[0] - _VIEW_ENUM_I
       log(f"ViewPrinter index {view_index} for {actual_val.type}")
-      return ViewPrinter(actual_val, _pretty_printers[base_key]["views"][view_index])
-      
+      assert printer is not None, f"Type {actual_val_type_str} has no printer."
+      return ViewPrinter(actual_val, printer["views"][view_index])
+
+  printer = _match_printer(type_str)
+  if printer is not None:
+    log(f"Matched printer for type: {type_str}")
+    return DefaultPrinter(val, printer)
+
+  if val.type.code in (gdb.TYPE_CODE_STRUCT, gdb.TYPE_CODE_UNION):
+    log(f"DefaultPrinter for struct/union type: {type_str}")
+    return DefaultPrinter(val)
+
+  if val.type.code == gdb.TYPE_CODE_ARRAY:
+    log(f"ArrayPrinter for array type: {type_str}")
+    return ArrayPrinter(val)
+
   log(f"No printer found for type: {type_str}")
   return None
 
